765bff2ad7fb5918ce23abff82515fcf3ba10fd0
Marcio Lima Inácio <marcio.lima.inacio@gmail.com>
2018-09-19 17:30:37 -0300

    Corrigindo outros caracteres não-suportados e MWE

--- src/apertium/parse_file.sh
+++ src/apertium/parse_file.sh
@@ -48,17 +48,28 @@ export LC_ALL="en_US.UTF-8"
 # Tag input
 read data
 data=$(echo $data | sed 's/@-@/-/g') # Replace Moses format @-@ to -
-data=$(echo $data | sed 's/\(\w\)\s\$\s\([[:digit:]]\)/\1 \&crf; \2/g') # Change currency from format US $ 1 to US &cfr; 1
+data=$(echo $data | sed 's/\(\w\)\s\$\s\([[:digit:]]\)/\1 \&crf; \2/g') # Change currency from format US $ 1 to US &crf; 1
 data=$(echo $data | sed 's/>/~/g') # Replace > to ~
 data=$(echo $data | sed 's/\//\&frasl;/g') # Replace / to &frasl;
 data=$(echo $data | sed 's/--/-/g') # Replace -- to -
+data=$(echo $data | sed 's/–/-/g') # Replace dash (–) to -
+data=$(echo $data | sed 's/“/"/g') # Replace Left Double Quotation Mark (“) to "
+data=$(echo $data | apertium-destxt)
 analysis_output=$(echo $data | lt-proc -a $SCRIPTPATH/apertium-$LANG/$LANG.automorf_retratos.bin)
 tagger_output=$(echo $analysis_output | apertium-tagger -g $SCRIPTPATH/apertium-$LANG/$LANG.prob)
+tagger_output=$(echo $tagger_output | apertium-retxt)
 tagger_output=$(echo $tagger_output | sed 's/~/^>$/g') # Replace ~ back to > and add token delimiters
 tagger_output=$(echo $tagger_output | sed 's/\"/\^\"\$/g') # Add start and end of token to upper commas
 tagger_output=$(echo $tagger_output | sed 's/#\s/$ ^ /g') # Add start and end of token to # tokens
-tagger_output=$(echo $tagger_output | sed 's/\s-\s/ ^-$ /g') # Add start and end of token to - tokens
+tagger_output=$(echo $tagger_output | sed 's/-\s/ ^-$ /g') # Add start and end of token to - tokens
+tagger_output=$(echo $tagger_output | sed 's/%\s/ ^%$ /g') # Add start and end of token to % tokens
+tagger_output=$(echo $tagger_output | sed 's/+\s/ ^+$ /g') # Add start and end of token to + tokens
 tagger_output=$(echo $tagger_output | sed 's/&\^\*frasl\$\^;<sent>\$/\^\/\$/g') # Replace &frasl; back to / and add token delimiters
-tagger_output=$(echo $tagger_output | sed 's/&\^crf<sig>\$\^;<sent>\$/\^cfr\$/g') # Replace &crf; with crf and add token delimiters
+tagger_output=$(echo $tagger_output | sed 's/&\^[*]\?crf\(<sig>\)\?\$\^;<sent>\$/\^cfr\$/g') # Replace &crf; with crf and add token delimiters
 tagger_output=$(echo $tagger_output | sed 's/\s&\s/ ^\&$ /g') # Add start and end of token to & tokens
+tagger_output=$(echo $tagger_output | sed 's/+o seu\([^$]*\$\)/+o\1 ^seu\1/g') # Divide "o seu" into two tokens
+tagger_output=$(echo $tagger_output | sed 's/+o qual\([^$]*\$\)/+o\1 ^qual\1/g') # Divide "o seu" into two tokens
+tagger_output=$(echo $tagger_output | sed 's/+o nosso\([^$]*\$\)/+o\1 ^nosso\1/g') # Divide "o seu" into two tokens
 echo $tagger_output
+
+# TODO: LIDAR COM PORCENTAGEM
