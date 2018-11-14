6e3ac6dfbbfd3f7034dd7c74443565a98759c57b
Superar <marcio.lima.inacio@gmail.com>
2018-11-14 13:05:12 -0200

    Script para teste de identificação de erro em dois passos

--- src/error_identification/tests/k_fold_hierarchical.py
+++ src/error_identification/tests/k_fold_hierarchical.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
+import numpy as np
+from sklearn.model_selection import KFold
 from sklearn.preprocessing import LabelEncoder
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import Perceptron
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.metrics import classification_report
 from readers.read_blast import BlastReader
 from readers.read_giza import GIZAReader
 from utils import *
@@ -11,20 +19,63 @@ FEATURES_FILE = 'features_final.pkl'
 TW_SZ = 5
 ERRORS = ['lex-incTrWord', 'lex-notTrWord']
 
-lb = LabelEncoder()
+lb_step1 = LabelEncoder()
+lb_step2 = LabelEncoder()
 
-def test_two_steps(data):
-    data['target'] = lb.fit_transform(data['target'])
 
-    data_step1 = data
-    data_step1.loc[data_step1['target'] != 'correct', 'target'] = 'error'
-    data_step1['target'] = lb.transform(data_step1['target'])
+def test_two_steps(data, model):
+    X = data.loc[:, data.columns != 'target']
+    y = data['target']
 
-    X_step1 = data_step1.loc[:, data.columns != 'target']
-    y_step1 = data_step1['target']
+    kf = KFold(n_splits=10, shuffle=True)
+    num_fold = 0
+    for (train, test) in kf.split(X):
+        num_fold += 1
+        print('Fold {}'.format(num_fold))
 
-    print(X_step1)
+        X_step1_train = X.loc[train]
+        y_step1_train = y.loc[train]
+        y_step1_train.loc[y_step1_train != 'correct'] = 'error'
+        y_step1_train = lb_step1.fit_transform(y_step1_train)
 
+        model_step1 = model
+        model_step1.fit(X_step1_train, y_step1_train)
+
+        X_step2_train = X.loc[train]
+        y_step2_train = y.loc[train]
+        X_step2_train = X_step2_train[y.loc[train] != 'correct']
+        y_step2_train = y_step2_train[y.loc[train] != 'correct']
+        lb_step2.fit(y)
+        print('Classes: {}'.format(lb_step2.classes_))
+        y_step2_train = lb_step2.transform(y_step2_train)
+
+        model_step2 = model
+        model_step2.fit(X_step2_train, y_step2_train)
+
+        X_test = X.loc[test]
+        y_step1_test = y.loc[test]
+        y_step2_test = y.loc[test]
+
+        results_step1 = model_step1.predict(X_test)
+        y_step1_test.loc[y_step1_test != 'correct'] = 'error'
+        y_step1_test = lb_step1.transform(y_step1_test)
+        print(classification_report(y_step1_test, results_step1))
+
+        error_lb = lb_step1.transform(['error'])[0]
+        X_step2_test = X_test[results_step1 == error_lb]
+
+        results_step2 = model_step2.predict(X_step2_test)
+        y_step2_test = y_step2_test[results_step1 == error_lb]
+        y_step2_test = lb_step2.transform(y_step2_test)
+        print(classification_report(y_step2_test, results_step2))
+
+        y_final_pred = results_step1[results_step1 != error_lb]
+        y_final_pred = np.hstack((y_final_pred, results_step2))
+
+        y_final_true = y_step1_test[results_step1 != error_lb]
+        y_final_true = np.hstack((y_final_true, y_step2_test))
+
+        print(classification_report(y_final_true, y_final_pred))
 
 def main():
     """Main function
@@ -97,7 +148,28 @@ def main():
 
     print('Iniciando treinamento')
     data = format_features(training_instances)
-    test_two_steps(data)
+
+    print('Arvore de Decisao - GINI')
+    test_two_steps(data, DecisionTreeClassifier())
+    print('------------------------')
+    print('Arvore de Decisao - Entropy')
+    test_two_steps(data, DecisionTreeClassifier(criterion='entropy'))
+    print('------------------------')
+    print('SVM')
+    test_two_steps(data, LinearSVC())
+    print('------------------------')
+    print('Perceptron')
+    test_two_steps(data, Perceptron(n_jobs=-1))
+    print('------------------------')
+    print('Random Forest - GINI')
+    test_two_steps(data, RandomForestClassifier(n_estimators=10))
+    print('------------------------')
+    print('Random Forest - Entropy')
+    test_two_steps(data, RandomForestClassifier(n_estimators=10, criterion='entropy'))
+    print('------------------------')
+    print('Naive Bayes')
+    test_two_steps(data, BernoulliNB())
+    print('------------------------')
 
 
 if __name__ == '__main__':
